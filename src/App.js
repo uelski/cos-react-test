146db18fd412fe45b28bd8b37e83ade23e6e65f0
Sam Vredenburgh <sam.vburgh@gmail.com>
2018-06-03 11:09:04 -0500

    created test component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Test from './Test.js'
 
 class App extends Component {
   render() {
@@ -8,6 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Mason Cos</h1>
+          <Test />
         </header>
       </div>
     );
